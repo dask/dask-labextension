a7cd3442b1b53e48a37cb305321c38c9c131863b
Ian Rose <ian.r.rose@gmail.com>
2018-08-14 14:34:02 -0700

    Remove a bunch of outdated stuff.

--- scripts/build_extension.js
+++
@@ -1,7 +0,0 @@
-var buildExtension = require('@jupyterlab/extension-builder').buildExtension;
-
-buildExtension({
-        name: 'dask-labextension',
-        entry: './lib/plugin.js',
-        outputDir: './dask_labextension/static'
-});
