22344ceb3178db24b94e39685a5ce8b24719776b
Ian Rose <ian.r.rose@gmail.com>
2019-04-13 10:09:05 -0700

    0.3.2 patch release for dask_labextension python package.

--- dask_labextension/__init__.py
+++ dask_labextension/__init__.py
@@ -6,7 +6,7 @@ from . import config
 from .clusterhandler import DaskClusterHandler
 from .dashboardhandler import DaskDashboardHandler
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 def _jupyter_server_extension_paths():
