3ab9d8fa5c760db944e0168bfd987ccb7973f614
Matthew Rocklin <mrocklin@gmail.com>
2019-07-31 17:41:10 -0700

    use dask=2.1 in CI

--- ci/install.sh
+++ ci/install.sh
@@ -8,12 +8,11 @@ conda update -q conda
 conda create -q -n test-environment python=$PYTHON
 source activate test-environment
 conda install -q \
+  dask=2.1 \
   pytest=3.7 \
   notebook
 
 # Install unreleased versions of dask and distributed for now
 # in order to get a patched config system.
-pip install git+https://github.com/dask/dask.git@677d62a35bae0fb964472b604bc52ef91b46ea22
-pip install git+https://github.com/dask/distributed.git@538767b4977d1bd14679ae555b7705088a7e5a16
 
 pip install -e .
