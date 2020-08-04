ee27f3be50b21b0fd0005c636cd027b4b893e492
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-08-04 10:31:22 +0100

    Remove failed sourcing

--- ci/python/install.sh
+++ ci/python/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export PATH="$HOME/miniconda/bin:$PATH"
-source /home/travis/.bashrc
 conda activate test-environment
 
 conda install -q \
--- ci/python/test.sh
+++ ci/python/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export PATH="$HOME/miniconda/bin:$PATH"
-source /home/travis/.bashrc
 conda activate test-environment
 
 python -m pytest dask_labextension
