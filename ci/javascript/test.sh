735bb3da770b65f46759055462e8886ac7f24cc2
Ian Rose <ian@coiled.io>
2021-01-06 16:13:23 -0800

    Remove old test infra.

--- ci/javascript/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH="$HOME/miniconda/bin:$PATH"
-conda activate test-environment
-
-jlpm build
-jlpm lint
-jlpm test
--- ci/python/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PATH="$HOME/miniconda/bin:$PATH"
-conda activate test-environment
-
-python -m pytest dask_labextension
