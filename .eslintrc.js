73cddec1856e7230890f14cc323e33044b1172d8
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-06-15 10:47:46 +0200

    Remove deprecated module in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:react/recommended'
   ],
   parser: '@typescript-eslint/parser',
